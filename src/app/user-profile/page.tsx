07cacc15866ce20275056510880c9b843ac200d7
Your Name <you@example.com>
2025-08-03 21:41:52 +0630

    profileCard

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -6,9 +6,12 @@ function Page() {
     return (
         <>
           <div className="w-screen h-screen relative overflow-hidden">
-               <div className="w-full h-5/12  bg-linear-to-r from-sky-500 to-indigo-500">
+               <div className="w-full h-5/12  bg-[url(/images/bg.svg)] bg-cover bg-center bg-no-repeat">
                </div>
-            <ProfileCard/>
+              <div className="px-10 md:px-0">
+                  <ProfileCard/>
+              </div>
+
           </div>
         </>
     )
--- src/components/ui/profileCard.tsx
+++ src/components/ui/profileCard.tsx
@@ -21,14 +21,14 @@ function ProfileCard() {
     return (
         <>
             <div
-                className="bg-white rounded-4xl shadow-2xl shadow-gray-500/50 p-6 w-2/3 h-8/12 mx-auto absolute top-1/5   left-1/2 transform -translate-x-1/2">
+                className="bg-white rounded-4xl shadow-2xl shadow-gray-500/50 p-6 w-full md:w-3/5 h-fit  mx-auto absolute top-1/5   left-1/2 transform -translate-x-1/2">
                 <div className="flex justify-between mt-5 relative">
                     <div className="flex gap-2">
                         <Users/>
                         <p>Connect</p>
                     </div>
                     <div
-                        className="rounded-full bg-green-600 w-[180px] h-[180px] overflow-hidden transform translate-y-[-110px] ">
+                        className="rounded-full bg-green-600 w-[130px] h-[130px] md:w-[180px] md:h-[180px] overflow-hidden absolute inset-0 top-[-110px]  mx-auto">
                         <Image src="/images/earth.jpg" alt="profile" width={120} height={120}
                                style={{width: "inherit", height: "inherit", objectFit: "cover"}}/>
                     </div>
@@ -39,8 +39,8 @@ function ProfileCard() {
                     </div>
 
                 </div>
-                <div className="flex justify-center items-center bg-green-600">
-                    <div>
+                <div className="flex justify-center items-center  mt-15">
+                    <div className="text-center">
                     <div>
                         <h1 className="text-2xl font-bold mt-5">John Doe</h1>
                         <p>New York, United States</p>
@@ -49,17 +49,17 @@ function ProfileCard() {
                         <p className="mt-5">Web Producer - Web Specialist.</p>
                         <p>John@gmail.com</p>
                     </div>
-                    <div>
+                    <div className="mb-5 flex justify-between gap-3 mt-5">
                         {
                             socialDetails.map((socialDetail, index) => (
                                 <div className="" key={index}>
-                                    <h2>{socialDetail.count}</h2>
+                                    <h2 className="font-semibold text-xl">{socialDetail.count}</h2>
                                     <p>{socialDetail.label}</p>
                                 </div>
                             ))
                         }
                     </div>
-                    <button>Log Out</button>
+                    <button className="bg-linear-to-r text-sm text-white text-shadow-md from-cyan-500 to-blue-500 py-2 px-6 rounded-full mb-6">Log Out</button>
                     </div>
                 </div>
             </div>
